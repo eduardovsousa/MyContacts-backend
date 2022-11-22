542cd2d72234bc12130ba2c62b27f64aab8bbf28
Eduardo Varela <eduardo.varela@outlook.com.br>
2022-11-22 02:11:15 -0300

    Caputando e tratando erros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,6 +8,10 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`🔥 Server started at http://localhost:${PORT}`));
